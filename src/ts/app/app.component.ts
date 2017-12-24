b2e988c88e3606cde0b4dbf4fa92195b5ab335e7
iphayao <phayaob@gmail.com>
2017-12-24 13:16:01 +0700

    Adding a Header to the Application

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: ` <div>
+                    <h1>{{header}}</h1>
+                </div> `,
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public header: string = "Color Tool";
 
 }
