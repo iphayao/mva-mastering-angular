3f1d8f73219bf2939a14c026052ea48d4f29c617
iphayao <phayaob@gmail.com>
2017-12-24 15:12:14 +0700

    Moving the Template to an External File

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,20 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: ` <div>
-                    <h1>{{header}}</h1>
-                    <ul>
-                        <li *ngFor="let color of colors">
-                            {{color}}
-                        </li>
-                    </ul>
-                    <form>
-                        <label for="new-color-input">New Color:</label>
-                        <input type="text" id="new-color-input" 
-                            name="newColor" [(ngModel)]="newColor">
-                    </form>
-                    <button type="button" (click)="addColor()">Add Color</button>
-                </div> `,
+    template: require("./app.component.html"),
 })
 export class AppComponent {
 
